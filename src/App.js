20666da608622df3e7acc4191dfc80e2748ce801
crushoftheyear <kajsalovisaholmstrom@gmail.com>
2020-04-28 21:13:10 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import 'normalize.css'
+import 'sass/app.scss'
+
+import { Header } from 'components/Header'
 
 export const App = () => {
   return (
-    <div>
-      Find me in src/app.js!
+    <div className="wrapper">
+      <Header />
     </div>
   )
 }
